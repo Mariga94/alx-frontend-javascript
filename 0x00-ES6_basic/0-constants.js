bd88ebe8b67f56f300512324a27f4db7dc3e2f5a
joseph <jsphmariga@gmail.com>
2022-12-19 22:17:07 +0000

    Instantiate variables

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,16 @@
-#!/usr/bin/node
 const taskFirst = () => {
   const task = 'I prefer const where I can.';
   return task;
 }
 
-export function getLast() {
+function getLast() {
   return ' is okay';
 }
 
 let taskNext = () => {
   let combination = 'But sometimes let';
   combination += getLast();
-  return coombination;
+  return combination;
 }
 
-export { taskFirst, taskNext }
+export { taskFirst, taskNext, getLast }
