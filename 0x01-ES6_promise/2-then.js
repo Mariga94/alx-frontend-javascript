6f431716f3bb5fb30e6a4f9d2d51150e94954a47
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2022-12-22 00:41:53 +0300

    Catch me if you can

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(
+    { status: 200, body: "Success" },
+    console.log("Got a response from the API"),
+    () => new Error("")
+  );
+}
