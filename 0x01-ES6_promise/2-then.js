4ab6904bf63c7f5237f0a56e50f5cdf2bcd605f9
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2022-12-22 00:45:06 +0300

    Catch errors

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(
     { status: 200, body: "Success" },
-    console.log("Got a response from the API"),
-    () => new Error("")
+    console.log("Got a response from the API")
   );
+  promise.catch(new Error(""));
 }
