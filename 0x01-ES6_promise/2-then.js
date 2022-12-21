b84ff9878fc4e6646b402b2ac1cdcc969ef1672f
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2022-12-22 01:19:54 +0300

    Add handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    { status: 200, body: "Success" },
-    console.log("Got a response from the API")
-  );
+  promise.then({ status: 200, body: "Success" });
   promise.catch(new Error(""));
+  promise.then(console.log("Got a response from the API"));
 }
