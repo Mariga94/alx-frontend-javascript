8c7e0c7c605fb3e28966e2d048f0325f31f4e136
joseph <jsphmariga@gmail.com>
2022-12-22 00:30:58 +0000

    Fix linting

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then({ status: 200, body: "Success" });
-  promise.catch(new Error(""));
-  promise.then(console.log("Got a response from the API"));
+  promise.then({ status: 200, body: 'Success' });
+  promise.catch(new Error(''));
+  promise.then(console.log('Got a response from the API'));
 }
