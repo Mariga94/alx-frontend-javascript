943605571f10fd161f159a1a87232698a546ead1
joseph <jsphmariga@gmail.com>
2022-12-22 00:45:55 +0000

    Fix linting

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,5 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((res) =>
-    console.log(res[0].body, res[1].firstName + " " + res[1].lastName)
-  );
+  Promise.all([uploadPhoto(), createUser()]).then((res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`));
 }
