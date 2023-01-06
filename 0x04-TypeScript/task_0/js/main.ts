2d14a7ff88f7e4e1bc86cabd2a56e1a070dc31ad
joseph <jsphmariga@gmail.com>
2023-01-06 13:25:16 +0000

    Add files

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,26 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+  location: "Barbados",
+};
+
+const student2: Student = {
+  firstName: "Lucy",
+  lastName: "Chan",
+  age: 24,
+  location: "Jamaica",
+};
+
+let studentsList:any[] = [student1, student2];
+
+function tableCreate() {
+  
+  };
